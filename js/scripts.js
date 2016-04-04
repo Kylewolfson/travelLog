7f77b9080b4811e3b472a1bfff560f0317711bae
Anthony Gold and Kyle Wolfson <kyle@>
2016-04-04 13:43:09 -0700

    penultimate code draft

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,26 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPlace = $("input#new-place").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedTime = $("input#new-time-of-year").val();
+    var inputtedNote = $("input#new-notes").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newPlace = new Location(inputtedPlace, inputtedLandmark, inputtedTime, inputtedNote);
 
-    $("ul#places").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#places").append("<li><span class='location-class'>" + newPlace.placeFoo() + "</span></li>");
 
-    $("input#new-first-name").val();
-    $("input#new-last-name").val();
+    $("input#new-place").val();
+    $("input#new-landmark").val();
+    $("input#new-time-of-year").val();
+    $("input#new-notes").val();
 
     $(".contact").last().click(function(){
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeFoo);
+      $(".place-name").text(newPlace.placeFoo);
+      $(".landmarks").text(newPlace.landmarkFoo);
+      $(".time-of-year").text(newPlace.placeFoo);
+      $(".notes").text(newPlace.landmarkFoo);    });
   });
 });
